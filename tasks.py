f1e7867323703830961dd11750208cdbc211ed44
Seth Fischer <seth@fischer.nz>
2025-07-06 16:56:27 +1200

    fix: tighter fit for europlan tab spacers

--- tasks.py
+++ tasks.py
@@ -81,14 +81,20 @@ def drawer_spacer_tab(c):
     """Drawer spacer for Europlan TAB Pedestal Drawers."""
     slug = "drawer-spacer-tab"
 
-    dr_width = 325
-    dr_depth = 448
+    dr_width = 326.5
+    dr_depth = 449.5
     chamf_rad = 1.0
+    tolerance = 0.25
 
     from cqgridfinity import GridfinityDrawerSpacer
 
     spacers = GridfinityDrawerSpacer(
-        dr_width, dr_depth, verbose=True, show_arrows=False, chamf_rad=chamf_rad
+        dr_width,
+        dr_depth,
+        verbose=True,
+        show_arrows=False,
+        chamf_rad=chamf_rad,
+        tolerance=tolerance,
     )
     spacers.render_half_set()
     spacers.save_step_file(output(slug, length=dr_width, width=dr_depth).as_posix())
